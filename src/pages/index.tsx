7357b3262524a1260a8f9b954a5dc2da88c2741d
Brian <patinobrian@gmail.com>
2021-10-25 23:46:43 -0400

    change message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to DEGEN!
         </h1>
 
         <p className={styles.description}>
