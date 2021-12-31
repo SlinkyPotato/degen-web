695c7a9f0dc36e2dd6892b2e82fb4f8361da477b
Sweets Sweetman <sweetmantech@gmail.com>
2021-12-31 10:51:50 -0500

    I init dependency array to ensure saving to db only happens once

--- apps/degen-web-ui/pages/connect/index.tsx
+++ apps/degen-web-ui/pages/connect/index.tsx
@@ -31,7 +31,7 @@ const Connect = () => {
     })
       .then((res) => res.json())
       .then((result) => console.log(result));
-  });
+  }, []);
 
   return (
     <GridContainer className="py-6">
