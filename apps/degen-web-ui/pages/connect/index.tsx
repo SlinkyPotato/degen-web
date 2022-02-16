351fa3a456805b80270f1118e4cb429762dc68a7
Sweets Sweetman <sweetmantech@gmail.com>
2022-02-15 21:20:25 -0300

    I fix type casting issues in connect page.

--- apps/degen-web-ui/pages/connect/index.tsx
+++ apps/degen-web-ui/pages/connect/index.tsx
@@ -1,23 +1,25 @@
 import { useWeb3React } from '@web3-react/core';
 import { useSession } from 'next-auth/react';
 import { SessionStatus } from '../../src/core/enums/auth.enums';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { GridContainer } from '../../src/shared/components/layout/grid-container';
 import { Box } from '@chakra-ui/react';
 import ActivatedDiscordPage from './ActivatedDiscordPage';
+import { DiscordSession } from '../../src/core/interfaces/auth-session';
 
 const Connect = () => {
   const { account, active } = useWeb3React();
   const { data: session, status } = useSession();
+  const { user } = (session as DiscordSession);
 
   useEffect(() => {
-    if (active && session?.user?.id) {
-      fetch(`/api/wallet/connect`, {
+    if (active && (user?.id)) {
+      fetch('/api/wallet/connect', {
         method: 'POST',
         body: JSON.stringify({
           account,
-          userId: session.user.id,
-          username: session.user.name,
+          userId: user?.id,
+          username: user.name,
         }),
         headers: new Headers({
           'Content-Type': 'application/json',
@@ -25,7 +27,7 @@ const Connect = () => {
         }),
       });
     }
-  }, [active, session, account]);
+  }, [active, account]);
 
   return (
     <GridContainer className="py-6">
